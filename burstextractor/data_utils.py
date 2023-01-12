27e076d2800ac4ee1e28062f57bcf29cce15dd50
Vincenzo Timmel <vincenzo.timmel@gmal.com>
2023-01-12 16:53:49 +0100

    Created burstlist, eda notebook.

--- burstextractor/data_utils.py
+++ burstextractor/data_utils.py
@@ -4,12 +4,8 @@ def explode_instruments_long_clean_instruments(data):
     data['instruments'] = data['instruments'].str.split(',')
     data = data.explode('instruments')
     data['instruments'] = data['instruments'].str.strip()
-    data['instruments'] = data['instruments'].replace({
-        '[': '',
-        ']': '',
-        '(' : '',
-        ')' : '',
-    })
+    for char in ['[', ']', '(', ')']:
+        data['instruments'] = data['instruments'].str.replace(char, '', regex=False)
     return data.reset_index(drop=True)
 
 def keep_only_type_I_to_VI(data):
--- spectogram_utils.py
+++ spectogram_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import cv2
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 def int_time_delta_to_pd_timedelta(int_time_delta):
     return pd.Timedelta(seconds=int_time_delta)
@@ -13,7 +14,7 @@ def spec_time_to_pd_datetime(spec):
     return datetime_range
     
 def spec_to_pd_dataframe(spec):
-    return pd.DataFrame(spec.data.T, index=spec_time_to_pd_datetime(spec), columns=spec.freq_axis)
+    return pd.DataFrame(spec.data.T, index=spec_time_to_pd_datetime(spec), columns=[str(x) for x in spec.freq_axis])
 
 def change_resolution_over_time(spectogram, time_resolution, interpolation=cv2.INTER_LINEAR):
     """
@@ -31,8 +32,11 @@ def change_resolution_over_time(spectogram, time_resolution, interpolation=cv2.I
     
     return pd.DataFrame(spectogram_resized, index=date_range, columns=columns)
 
-def plot_spectogram(spectogram, max_x_labels=30):
+def plot_spectogram(spectogram):
     spectogram = spectogram.copy()
     spectogram.index = spectogram.index.strftime('%Y-%m-%d %H:%M:%S')
-    sns.heatmap(spectogram.T, cmap='viridis', cbar_kws={'label': 'Flux (W/m^2)'})
-    
+    ax = sns.heatmap(spectogram.T, cmap='viridis', cbar_kws={'label': 'Flux (W/m^2)'})
+    plt.title('Spectogram')
+    plt.ylabel('Frequency (MHz)')
+    plt.xlabel('Time UTC')
+    plt.show()
