9a906cebe4a61e6af66bc0d143fb4e30d57aa0bc
Vincenzo Timmel <vincenzo.timmel@gmal.com>
2023-02-20 14:42:14 +0100

    Fixed mistakes in promt.

--- load_data_into_database.py
+++ load_data_into_database.py
@@ -12,12 +12,12 @@ from database_utils import *
 LOGGER = logging_utils.setup_custom_logger("database_data_addition")
 
 
-def main(start_date, end_date, instrument, dir):
+def main(start_date, end_date, instrument_glob_pattern, dir):
     paths = glob_files(
         dir_path=dir,
         start_date=start_date,
         end_date=end_date,
-        file_name_pattern=instrument,
+        file_name_pattern=instrument_glob_pattern,
     )
     dict_paths = create_dict_of_instrument_paths(paths)
 
@@ -41,14 +41,14 @@ def main(start_date, end_date, instrument, dir):
 
 if __name__ == "__main__":
     ## Example:
-    # python load_data_into_database.py --start_date 2020-01-01 --end_date 2020-01-02 --instrument_glob_pattern *
+    # python load_data_into_database.py --start_date 2022-01-01 --end_date 2023-02-20 --instrument_glob_pattern "all"
     # Get arguments from command line
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--start_date", type=str, default=(datetime.today().date() - timedelta(days=14))
     )
     parser.add_argument("--end_date", type=str, default=datetime.today().date())
-    parser.add_argument("--instrument_glob_pattern", type=str, default="*")
+    parser.add_argument("--instrument_glob_pattern", type=str, default="all")
     parser.add_argument("--dir", type=str, default=LOCAL_DATA_FOLDER)
     args = parser.parse_args()
     LOGGER.info(
@@ -57,6 +57,11 @@ if __name__ == "__main__":
     # Update date to datetime
     args.start_date = datetime.strptime(args.start_date, "%Y-%m-%d").date()
     args.end_date = datetime.strptime(args.end_date, "%Y-%m-%d").date()
+    args.instrument_glob_pattern = (
+        "*"
+        if args.instrument_glob_pattern.lower() in ["all"]
+        else args.instrument_glob_pattern
+    )
     try:
         # Main
         main(**vars(args))
